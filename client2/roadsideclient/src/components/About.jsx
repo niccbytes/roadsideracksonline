de20d3b571ee53d8a582301abdbd08fc26f5c504
gracecatk <gracie.kiker@gmail.com>
2024-02-07 18:30:51 -0600

    lost in the sauce rn

--- client2/roadsideclient/src/components/About.jsx
+++ client2/roadsideclient/src/components/About.jsx
@@ -1,14 +1,20 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Container, Text, Heading, Box } from '@chakra-ui/react';
 
-// ...
-
-import { Link as ChakraLink } from '@chakra-ui/react';
-import { Link as RouterLink } from 'react-router-dom';
-
-export default function About() {
+const About = () => {
   return (
-    <ChakraLink as={RouterLink} to="/about" color="white">
-      <Button variant="link">About</Button>
-    </ChakraLink>
+    <Container centerContent>
+      <Heading as="h1" size="2xl" my={4}>About</Heading>
+      <Text fontSize="lg" my={4}>
+        We are a company that specializes in [describe what your company does].
+        Our mission is to [describe your company's mission].
+        We were founded in [year] and have grown to become [describe growth or achievements].
+        Our team consists of [describe your team], and we pride ourselves on [mention unique qualities or values].
+        We offer [list of products or services], and our customers appreciate us for [mention customer appreciation or feedback].
+      </Text>
+      {/* Add more sections as needed, such as a team section, product gallery, etc. */}
+    </Container>
   );
-}
+};
+
+export default About;
